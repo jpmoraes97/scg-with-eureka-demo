c5c902d6cdfa9879820b4f05038ce2738029fe90
casjom <joao.moraes@capela.com.br>
2021-07-09 11:13:52 -0300

    eureka server created

---
+++ eureka-server/src/main/kotlin/br/com/moraesit/eurekaserver/EurekaServerApplication.kt
@@ -0,0 +1,11 @@
+package br.com.moraesit.eurekaserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EurekaServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<EurekaServerApplication>(*args)
+}
