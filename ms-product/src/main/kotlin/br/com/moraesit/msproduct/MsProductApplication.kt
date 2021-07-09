bc56b835c22dac40fb784d26762ac84e8898ea6a
casjom <joao.moraes@capela.com.br>
2021-07-09 11:31:12 -0300

    ms product created

---
+++ ms-product/src/main/kotlin/br/com/moraesit/msproduct/MsProductApplication.kt
@@ -0,0 +1,11 @@
+package br.com.moraesit.msproduct
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsProductApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsProductApplication>(*args)
+}
---
+++ ms-product/src/test/kotlin/br/com/moraesit/msproduct/MsProductApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.moraesit.msproduct
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsProductApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
