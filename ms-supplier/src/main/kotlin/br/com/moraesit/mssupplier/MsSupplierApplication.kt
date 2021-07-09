c451cc3d2ded0c0406f76bbb18ff67c04c7e6f77
casjom <joao.moraes@capela.com.br>
2021-07-09 11:45:03 -0300

    ms supplier created

---
+++ ms-supplier/src/main/kotlin/br/com/moraesit/mssupplier/MsSupplierApplication.kt
@@ -0,0 +1,11 @@
+package br.com.moraesit.mssupplier
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsSupplierApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsSupplierApplication>(*args)
+}
---
+++ ms-supplier/src/test/kotlin/br/com/moraesit/mssupplier/MsSupplierApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.moraesit.mssupplier
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsSupplierApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
