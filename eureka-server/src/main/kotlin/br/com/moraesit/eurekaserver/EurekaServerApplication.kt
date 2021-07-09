24e08cd5cbfc5355f4f30d58ba2d3e0b1a759805
casjom <joao.moraes@capela.com.br>
2021-07-09 11:25:31 -0300

    eureka server configured

--- eureka-server/src/main/kotlin/br/com/moraesit/eurekaserver/EurekaServerApplication.kt
+++ eureka-server/src/main/kotlin/br/com/moraesit/eurekaserver/EurekaServerApplication.kt
@@ -2,10 +2,12 @@ package br.com.moraesit.eurekaserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class EurekaServerApplication
 
 fun main(args: Array<String>) {
-	runApplication<EurekaServerApplication>(*args)
+    runApplication<EurekaServerApplication>(*args)
 }
