0848630726404bad86ba3eeb502cf4f33fca124d
casjom <joao.moraes@capela.com.br>
2021-07-09 12:09:32 -0300

    gateway service configured using property based configuration

--- gateway-service/src/main/kotlin/br/com/moraesit/gatewayservice/GatewayServiceApplication.kt
+++ gateway-service/src/main/kotlin/br/com/moraesit/gatewayservice/GatewayServiceApplication.kt
@@ -2,10 +2,12 @@ package br.com.moraesit.gatewayservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class GatewayServiceApplication
 
 fun main(args: Array<String>) {
-	runApplication<GatewayServiceApplication>(*args)
+    runApplication<GatewayServiceApplication>(*args)
 }
